1f5d4fe65012975af21d597e0d6f9261613f170b
mehdi mahdavi <mehdimahdavi73@gmail.com>
2021-06-05 13:20:32 +0430

    video 13

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {selectedSong} from '../actions'
+import { selectedSong } from '../actions'
 import SongList from './SongList';
 
 
-export default ()=>{
+export default () => {
 
-  return(
-    <>
+  return (
+    <div className="ui container grid" style={{marginTop: '10px'}}>
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
 
-    <SongList/>
-    
-    </>
+      </div>
+    </div>
   );
 
 };
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 
 class SongList extends React.Component {
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            select
+                        </button>
+                    </div>
+                    <div className="content">
+                        {song.title}
+                    </div>
+                </div>
+            );
+        });
+    }
+
+
     render() {
 
-//        console.log(this.props)
+        //        console.log(this.props)
         return (
-            <div>
-                SongList
+            <div className="ui middle aligned  devided list">
+                {this.renderList()}
             </div>
         )
 
     }
 }
 
-const mapStoreToProps=(state)=>{
-//console.log(state);
-return {songs:state.songs}
+const mapStoreToProps = (state) => {
+    //console.log(state);
+    return { songs: state.songs }
 }
 
 export default connect(mapStoreToProps)(SongList);
