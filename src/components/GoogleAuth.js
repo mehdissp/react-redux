565bc73e537922f7cb0aa5910623c07b1145a7a1
mehdi mahdavi <mehdimahdavi73@gmail.com>
2021-06-10 16:48:08 +0430

    video 42

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
 
 export default class GoogleAuth extends Component {
+    state={isSignedIn:null};
 
     componentDidMount() {
         window.gapi.load('client:auth2',()=>{
             window.gapi.client.init({
                 clientId:'1039543142952-pmerev6ijvmqp1ujud2pkjvp5letns0l.apps.googleusercontent.com',
                 scope:'email'
-            })
+            }).then(()=>{
+                   this.auth=window.gapi.auth2.getAuthInstance();
+                   this.setState({isSignedIn:this.auth.isSignedIn.get()});
+            });
         })
+    }
+
+    renderAuthButton(){
+        // if (this.state.isSignedIn===null) {
+        //     return <div> نمیدونم وضعیتمو</div>
+        // }
+         if(this.state.isSignedIn){
+            return <div> sign in</div>
+        }
+        else{
+            return <div> not sign in</div>
+        }
+
     }
     
     render() {
         return (
             <div>
-                GoogleAuth
+                {this.renderAuthButton()}
             </div>
         )
     }
