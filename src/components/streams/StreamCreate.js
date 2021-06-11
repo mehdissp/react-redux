f46eb885dba540c90b46d45e97300b6c80b485e5
mehdi mahdavi <mehdimahdavi73@gmail.com>
2021-06-11 15:59:26 +0430

    video 57

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -19,13 +19,19 @@ class StreamCreate extends React.Component {
 
         )
     }
+    onSubmit = (formValues) => {
+        console.log(formValues);
+    }
     render() {
-
+            console.log(this.props)
         return (
-            <form className="ui form">
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
 
                 <Field name="title" component={this.renderInput} label="Title" />
                 <Field name="description" component={this.renderInput} label="Description" />
+                <button className="button ui primary">
+                    Submit
+                </button>
             </form>
         )
     }
