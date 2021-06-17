2cb273e93c63ce4a37e02780ff0584d9ef6aaf71
mehdi mahdavi <mehdimahdavi73@gmail.com>
2021-06-17 15:36:57 +0430

    video 65

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,13 @@
-import { SIGN_IN, SIGN_OUT,CREATE_STREAM } from './types';
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+
+} from './types';
 import streams from '../apis/streams';
 
 export const signIn = (userId) => {
@@ -27,4 +36,35 @@ export const createStream = formValues => async dispatch => {
         type: CREATE_STREAM,
         payload: response.data
     })
+}
+
+
+// Rest api
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({
+        type: FETCH_STREAMS,
+        payload: response.data
+    });
+};
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({
+        type: FETCH_STREAMS,
+        payload: response.data
+    });
+};
+
+export const editStream=(id,formValues)=> async dispatch => {
+    const response = await streams.put(`/streams/${id}`,formValues);
+    dispatch({
+        type: EDIT_STREAM,
+        payload: response.data
+    });
+}
+
+export const deleteStream =(id)=> async dispatch => {
+     await streams.delete(`/streams/${id}`)
 }
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,15 +1,25 @@
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+
+} from '../actions/types';
+
 const INITIAL_STATE = { 
         isSignedIn: null,
         userId:null,
-
 }
 
 export default (state = { INITIAL_STATE }, action) => {
     switch (action.type) {
-        case 'SIGN_IN':
+        case SIGN_IN:
 
             return { ...state, isSignedIn: true,userId:action.payload }
-        case 'SIGN_OUT':
+        case SIGN_OUT:
 
             return { ...state, isSignedIn: false,userId:null }
         default:
